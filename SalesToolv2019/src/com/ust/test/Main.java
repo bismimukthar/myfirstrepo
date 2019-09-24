000d0f5525435fa10ea88775048e372434750766
user62 <user62@user62-desktop>
2019-09-24 14:57:23 +0530

    Intial commit

---
+++ SalesToolv2019/src/com/ust/test/Main.java
@@ -0,0 +1,15 @@
+package com.ust.test;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		System.out.println("welcome to sales app!");
+		displayGreeting();
+		
+	}
+	public static void displayGreeting()
+	{
+		System.out.println("success");
+	}
+
+}
---
+++ SalesToolv2019/src/com/ust/test/SalesData.java
@@ -0,0 +1,5 @@
+package com.ust.test;
+
+public class SalesData {
+
+}
